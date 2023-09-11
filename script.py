cc84fa65afec675155b1457a9fa18361330941d3
Lucas Catto <lucascattosilva2018@gmail.com>
2023-09-11 15:51:35 -0700

    Much better now, and working!!

--- script.py
+++ script.py
@@ -5,36 +5,39 @@ from random import randint
 def line (size=80):
     for c in range(0, size):
         print("=", end='')
-        sleep(0.01)
     print("")
 
 
 line()
-text1 = "| Hello, I'm your computer and I thought a number between 1 and 100!"
+text1 = "| Hello, I'm your computer and I thought a number between 1 and 100!           |"
 for t in text1:
     print(t, end='')
-    sleep(0.02)
+    sleep(0.03)
 print()
 
 computer = randint (0, 100)
 
 while True:
     print("| " + ("-" * 76) + " |")
-    user = int(input("| Try to guess: "))
+    user = int(input("  Try to guess: "))
 
     if(0 < user <= 100):
 
         if (user == computer):
-            print("| You won!")
+            print("| You won!", end='')
+            print(" " * 69 + "|")
             line()
             break
         elif (user > computer):
-            print("| Lower")
+            print("| Lower", end='')
+            print(" " * 72 + "|")
         else:
-            print("| Higher")
+            print("| Higher", end='')
+            print(" " * 71 + "|")
 
     else:
-        print("Invalid number!")
+        print("| Invalid number!", end='')
+        print(" " * 62 + "|")
 
 v1 = "|     \\"
 v2 = "|     (o)>"
@@ -43,19 +46,26 @@ v4 = "|     - -"
 
 for text in v1:
     print(text, end='')
-    sleep(0.07)
+    sleep(0.1)
 print()
 for text in v2:
     print(text, end='')
-    sleep(0.07)
+    sleep(0.1)
 print()
 for text in v3:
     print(text, end='')
-    sleep(0.07)
+    sleep(0.1)
 print()
 for text in v4:
     print(text, end='')
-    sleep(0.07)
+    sleep(0.1)
+print()
+
+vetor = "| Thanks for using GalloGame!"
+
+for letter in vetor:
+    print(letter, end='')
+    sleep(0.03)
 print()
 
 line()
